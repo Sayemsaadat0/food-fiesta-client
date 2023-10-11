ca88e700ac92a839c07188d6043b7d5fa6672cfe
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-12 00:11:01 +0600

    navbar

--- app/Components/Shared/Navbar/Navbar.tsx
+++ app/Components/Shared/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-300">
+    <div className="navbar ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -11,30 +11,36 @@ const Navbar = () => {
               className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
-              stroke="currentColor" >
+              stroke="currentColor"
+            >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16" />
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
             </svg>
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52" > 
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
             {menuItems}
           </ul>
         </div>
-        <Link
-          href="/"
-          className="normal-case text-2xl  tracking-widest font-semibold" >
-          Doctors Portal
-        </Link>
+        <div className="logo">
+          <Link
+            href="/"
+            className=" text-3xl logoText font-bold"
+          >
+            Food Fiesta
+          </Link>
+        </div>
       </div>
       {/*  */}
       <div className=" hidden lg:flex w-full  justify-end">
         <ul className="menu menu-horizontal lg:flex gap-10 px-1">
-          {menuItems}
+          {menuItems} 
         </ul>
       </div>
     </div>
@@ -46,7 +52,9 @@ export default Navbar;
 const menuItems = (
   <>
     <Link href="/">Home</Link>
-    <Link href="/appoinment">Appoinment</Link>
-    <Link href="/about">About</Link>
+    <Link href="">About</Link>
+    <Link href="">Contact</Link>
+    <Link href="">Career</Link>
+    <Link href="">Blogs</Link>
   </>
 );
